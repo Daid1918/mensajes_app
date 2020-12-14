9418e1f912cac5d4df2c06f1f5b46e00f4d62a3c
David <david_martinez1992@hotmail.com>
2020-12-14 14:31:46 -0600

    Agregando mas clases

---
+++ src/main/java/com/modelo/MensajeDAO.java
@@ -0,0 +1,17 @@
+
+package com.modelo;
+
+public class MensajeDAO {
+    
+    public static void crearMensajeDB(Mensajes mensaje){
+    }
+    
+    public static void leerMensajesDB(){
+    }
+    
+    public static void borrarMensajeDB(int id_mensaje){
+    }
+    
+    public static void actualizarMensajeDB(Mensajes mensaje){
+    }
+}
---
+++ src/main/java/com/modelo/MensajeService.java
@@ -0,0 +1,17 @@
+
+package com.modelo;
+
+public class MensajeService {
+ 
+    public static void crearMensaje(){
+    }
+    
+    public static void listarMensajes(){
+    }
+    
+    public static void borrarMensaje(){
+    }
+    
+    public static void editarMensaje(){
+    }
+}
